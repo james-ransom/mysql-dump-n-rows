1b37323e3e42dc2ce188a8f0c665e50e297e53c3
james ransom <43888908+james-ransom@users.noreply.github.com>
2018-12-16 16:04:18 -0500

    Update creds.php

--- creds.php
+++ creds.php
@@ -4,6 +4,7 @@ $servername = "127.0.0.1";
 $username = "[YOUR_DB_USERNAME]";
 $password = "[YOUR_PASSWORD]";
 $dbname = "[YOUR_DB_NAME";
+$max_rows_per_table = 1000; 
 
 // tables that contain these keywords will be ignored
 $ignore = array("tmp", "temp", "__", "meta", "mysql", "users");
