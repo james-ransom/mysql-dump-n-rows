fe86f194bee772a9378c74760bde0468ef8e6c9d
james ransom <43888908+james-ransom@users.noreply.github.com>
2018-12-16 16:00:29 -0500

    Create creds.php

---
+++ creds.php
@@ -0,0 +1,9 @@
+<?php
+
+$servername = "127.0.0.1";
+$username = "[YOUR_DB_USERNAME]";
+$password = "[YOUR_PASSWORD]";
+$dbname = "[YOUR_DB_NAME";
+
+// tables that contain these keywords will be ignored
+$ignore = array("tmp", "temp", "__", "meta", "mysql", "users");
